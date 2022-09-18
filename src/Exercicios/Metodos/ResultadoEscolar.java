5341d1b544eb347eb83aa672e3f5db8964add14a
Nelis Junior <nelis.arruda.097@ufrn.edu.br>
2022-09-18 10:00:49 -0300

    Aula: condicação ternária

--- src/Exercicios/Metodos/ResultadoEscolar.java
+++ src/Exercicios/Metodos/ResultadoEscolar.java
@@ -5,13 +5,8 @@ public class ResultadoEscolar {
     public static void main(String[] args){
         int nota = 6;
 
-        if(nota >= 7)
-            System.out.println("Alun@ aprovad@!");
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Aun@ está em recuperação!");
-
-        else
-            System.out.println("Alun@ reprovado!");
+        System.out.println(resultado);
     }
 }
